ad43ec396ada217bd884051527a97c5ae9c1b4b3
Sauce77 <sauceortediego505@gmail.com>
2025-02-14 11:55:59 -0600

    se realizo primera prueba

--- grcapi/settings.py
+++ grcapi/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.security.SecurityMiddleware',
 ]
 
 REST_FRAMEWORK = {
