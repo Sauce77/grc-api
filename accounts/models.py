dda21db188b71508980c7a1fa1ee0492aede00ab
Sauce77 <sauceortediego505@gmail.com>
2025-04-10 19:17:02 -0600

    se agrego attributo superuser

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,13 @@ from django.contrib.auth.models import User
 
 class Rol(models.Model):
     """
-        Identifica los privilegios de un usuario. UNUSED.
+        Identifica los privilegios de un usuario.
     """
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=150)
     descripcion = models.TextField(blank=True,null=True)
+
+    usuario = models.ManyToManyField(User)
     
     def __str__(self):
         return self.nombre
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,17 +6,18 @@ from .models import Rol
 class RolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rol
-        fields = ["id","nombre"]
+        fields = ["nombre"]
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["first_name","last_name","username", "email","password"]
+        exclude = ["id"]
 
 class AdminSerializer(serializers.ModelSerializer):
 
     admin_key = serializers.CharField(max_length=100)
+
     class Meta:
         model = User
-        fields = ["username","first_name","last_name","password","admin_key"]
+        exclude = ["id"]
